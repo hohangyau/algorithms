efba1a1018b3e28273a38ec5f1784c7ff28ab5c5
hohangyau <hohangyau@gmail.com>
2022-05-24 20:28:13 +0800

    revision

--- array/productExceptSelf.cpp
+++ array/productExceptSelf.cpp
@@ -5,32 +5,39 @@
 #include <unordered_map>
 #include <cstdint>
 using namespace std;
-    vector<int> productExceptSelf(vector<int>& nums) {
+
+   vector<int> productExceptSelf(vector<int>& nums) {
+
         int n = nums.size();
-        vector<int> res(n);
-        res[0] = 1;
+        vector<int> dp(n);
+        dp[0] = nums[0];
         
-        for(int i = 1; i < n; ++i) {
-            res[i] = nums[i-1] * res[i-1];
+        for(int i = 1; i < n; ++i){
+            dp[i] = nums[i] * dp[i-1];
         }
         
+        dp[n-1] = dp[n-2];
         int rightSoFar = nums[n-1];
-        for(int i = n-2; i >= 0; --i){
-            res[i] *= rightSoFar;
+        
+        for(int i = n-2; i>0; --i){
+            dp[i] = dp[i-1] * rightSoFar;
             rightSoFar *= nums[i];
         }
-   
-  
-        
-        return res;
-    }
+            
+        dp[0] = rightSoFar;
 
+        
+        return dp;
+    }
 int main (){
 
 	//vector<vector<char>> board {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
 	//bool res = exist(board, "AAB");
-
-	cout << res  << endl;
+	vector<int> vec {2,3,5,0};
+	vec = productExceptSelf(vec);
+	for(auto i : vec){
+		cout << i << " " << endl;
+	}
 
 
 	return 0;
